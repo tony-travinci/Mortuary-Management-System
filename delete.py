bb1450e974aa48c63b6b40c12dc0072f9304fd1d
Andrew-Wambugu <andrewwambugu93@gmail.com>
2017-06-27 00:01:02 +0300

    Delete file independently created

--- delete.py
+++ delete.py
@@ -8,15 +8,23 @@ try:
 except:
     raise ValueError("Contact System Admin. Database Offline")
 
+
 def delete():
     status = False
     try:
+        if StaffEntry.get() != "":
+            status = True
+        elif StaffEntry.get() == "":
+            messagebox.showinfo("ERROR","STAFF ID FIELD CANNOT BE EMPTY")
+            status=False
+    except:
+        status=False
+    try:
         cursor.execute("SELECT staffid FROM users where staffid ='%s'" % (StaffEntry.get()))
         staffid = cursor.fetchone()
-        staffidone = str(staffid[0])
-        print(staffidone)
+
         if staffid is not None:
-            messagebox.showinfo("SUCCESS", "USER EXISTS")
+            print("User exists")
             status = True
         else:
             messagebox.showinfo("ERROR","USER DOES NOT EXIST")
@@ -26,16 +34,32 @@ def delete():
 
     try:
         if status == True:
+            cursor.execute("SELECT enum FROM users WHERE staffid = '%s'"% (StaffEntry.get()))
+            enm=cursor.fetchone()
+            enm=str(enm[0])
+            if enm == 'Y':
+                messagebox.showinfo("ERROR","ADMIN CANNOT BE DELETED")
+                status = False
+            elif enm == 'N':
+                print("Normal user")
+                status = True
+    except:
+        status = False
+
+
+
+    try:
+        if status == True:
             cursor.execute("DELETE FROM users WHERE staffid = '%s'" % (StaffEntry.get()))
             conn.commit()
+            messagebox.showinfo("SUCCESS", "USER DELETED")
             print("User Deleted")
-            status = True
     except:
-        conn.rollback()
-        print("User not deleted")
-        status=False
-    if status == True
-        messagebox.showinfo()
+        if status == False:
+            conn.rollback()
+            print("User not deleted")
+            messagebox.showinfo("Error", "User was not deleted")
+
 
 
 root=Tk()
@@ -51,15 +75,18 @@ root.geometry("1366x768+0+0")
 ChangePassFrame=Frame(root, bg='Green', width=1366, height=768, bd=4, relief='ridge')
 ChangePassFrame.pack()
 
-TopLabelName=Label(ChangePassFrame, text="Change Password", bg='Green', font=('times', 14, 'italic'), fg='white')
-TopLabelName.place(x=400,y=0)
+TopLabelName=Label(ChangePassFrame, text="DELETE USER", bg='Green', font=('times', 18, 'italic'), fg='white')
+TopLabelName.place(x=400,y=200)
+
+NameLabel=Label(ChangePassFrame, bg='green', text="STAFF ID", fg='white', font=('times', 14, 'italic'))
+NameLabel.place(x=50,y=300)
 
 StaffEntry=Entry(ChangePassFrame, font=('times', 14, 'italic'), fg='black', width=50, textvariable=StaffVariable)
-StaffEntry.place(x=250,y=50)
+StaffEntry.place(x=250,y=300)
 
 
-SubmitButton=Button(ChangePassFrame, text="Submit", font=('times', 14, 'italic'), fg='Green', bd=2, relief='ridge', command=delete())
-SubmitButton.place(x=900,y=550)
+SubmitButton=Button(ChangePassFrame, text="Submit", font=('times', 14, 'italic'), fg='Green', bd=2, relief='ridge', command=delete)
+SubmitButton.place(x=700,y=400)
 
 
 
